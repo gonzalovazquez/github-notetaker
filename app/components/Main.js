3eda79f82c53b59406c3bc314b1096a720a83e5f
gonzalovazquez <gonzalo.segura1@gmail.com>
2015-08-15 21:39:36 -0400

    Added search transition to Profiles component

--- app/components/Main.js
+++ app/components/Main.js
@@ -1,5 +1,6 @@
 var React = require('react');
 var RouteHandler = require('react-router').RouteHandler;
+var SearchGithub = require('./SearchGithub');
 
 // Creates a new React component
 var Main = React.createClass({
@@ -8,7 +9,7 @@ var Main = React.createClass({
       <div className="main-container">
         <nav className="navbar navbar-default" role="navigation">
           <div className="col-sm-7 col-sm-offset-2" style={{marginTop: 15}}>
-            MENU
+            <SearchGithub />
           </div>
         </nav>
         <div className="container">
